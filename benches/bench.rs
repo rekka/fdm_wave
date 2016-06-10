5051d61f280193bb768f90ef9d0430f4eb2be005
Norbert Pozar <bertapozar@gmail.com>
2016-06-10 09:35:37 +0900

    First parallel impl using rayon and benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,63 +3,81 @@ extern crate test;
 extern crate ndarray;
 extern crate fdm_wave;
 
-use test::Bencher;
+mod bench {
+    mod ndarray {
+        use test::{black_box, Bencher};
+        use ndarray::Array;
+        use fdm_wave::*;
 
-use ndarray::Array;
+        fn bench_ndarray(b: &mut Bencher, ny: usize, nx: usize) {
+            let dim = (ny, nx);
+            let u = Array::from_elem(dim, 1.);
+            let v = black_box(Array::from_elem(dim, 1.));
+            let mut w = Array::from_elem(dim, 1.);
+            let mu = 2.32;
 
-use fdm_wave::*;
+            b.iter(|| {
+                wave_step_ndarray(&u, &v, &mut w, mu);
+            });
+        }
 
-fn bench_ndarray(b: &mut Bencher, ny: usize, nx: usize) {
-    let dim = (ny, nx);
-    let u = Array::from_elem(dim, 1.);
-    let v = test::black_box(Array::from_elem(dim, 1.));
-    let mut w = Array::from_elem(dim, 1.);
-    let mu = 2.32;
+        #[bench]
+        fn s128(b: &mut Bencher) {
+            bench_ndarray(b, 128, 128);
+        }
 
-    b.iter(|| {
-        wave_step_ndarray(&u, &v, &mut w, mu);
-    });
-}
+        #[bench]
+        fn s512(b: &mut Bencher) {
+            bench_ndarray(b, 512, 512);
+        }
 
-fn bench_simd(b: &mut Bencher, ny: usize, nx: usize) {
-    let dim = (ny, nx);
-    let l = dim.0 * dim.1;
-    let u = vec![1.; l];
-    let v = vec![1.; l];
-    let mut w = test::black_box(vec![1.; l]);
-    let mu = 2.32;
+        #[bench]
+        fn s2048(b: &mut Bencher) {
+            bench_ndarray(b, 2048, 2048);
+        }
+    }
 
-    b.iter(|| {
-        wave_step(&u, &v, &mut w, dim, mu);
-    });
-}
+    macro_rules! bench_step {
+        ($modname:ident, $func: ident, $(($name:ident, $ny:expr, $nx:expr))+) => {
+            mod $modname {
+                use test::{black_box, Bencher};
+                use fdm_wave::*;
+                $(
+                #[bench]
+                fn $name(b: &mut Bencher)
+                {
+                    let dim = ($ny, $nx);
+                    let l = dim.0 * dim.1;
+                    let u = vec![1.; l];
+                    let v = vec![2.; l];
+                    let mut w = black_box(vec![1.; l]);
+                    let mu = 2.32;
 
-#[bench]
-fn bench_ndarray_128(b: &mut Bencher) {
-    bench_ndarray(b, 128, 128);
-}
+                    b.iter(|| {
+                        $func(&u, &v, &mut w, dim, mu);
+                    });
+                }
+                )+
+            }
+        }
+    }
 
-#[bench]
-fn bench_ndarray_512(b: &mut Bencher) {
-    bench_ndarray(b, 512, 512);
-}
+    bench_step!{simd, wave_step,
+                (s128, 128, 128)
+                (s512, 512, 512)
+                (s2048, 2048, 2048)
+                }
 
-#[bench]
-fn bench_ndarray_2048(b: &mut Bencher) {
-    bench_ndarray(b, 2048, 2048);
-}
+    bench_step!{simd_single, wave_step_single,
+                (s128, 128, 128)
+                (s512, 512, 512)
+                (s2048, 2048, 2048)
+                }
 
-#[bench]
-fn bench_simd_128(b: &mut Bencher) {
-    bench_simd(b, 128, 128);
-}
+    bench_step!{simd_parallel, wave_step_parallel,
+                (s128, 128, 128)
+                (s512, 512, 512)
+                (s2048, 2048, 2048)
+                }
 
-#[bench]
-fn bench_simd_512(b: &mut Bencher) {
-    bench_simd(b, 512, 512);
-}
-
-#[bench]
-fn bench_simd_2048(b: &mut Bencher) {
-    bench_simd(b, 2048, 2048);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 //! Finite difference method for the wave equation with Neumann boundary data.
 #![feature(cfg_target_feature)]
 extern crate ndarray;
+extern crate rayon;
+
+#[cfg(test)]
 extern crate ndarray_rand;
+#[cfg(test)]
 extern crate rand;
+
 extern crate simd;
 #[cfg(target_feature = "avx")]
 use simd::x86::avx::f64x4;
 
-use ndarray::prelude::*;
+use ndarray::{Array, Ix};
 use std::cmp::min;
 
 pub type Array2d = Array<f64, (Ix, Ix)>;
@@ -50,6 +55,8 @@ pub fn wave_step_ndarray(w: &Array2d, u: &Array2d, v: &mut Array2d, mu: f64) {
 /// Performs one step of the finite difference scheme for the wave equation with zero Neumann
 /// boundary condition.
 ///
+/// The discretization is the standard central 2nd order difference in both space and time.
+///
 /// - `u`: value at step `n - 1`
 /// - `v`: value at step `n`
 /// - `w`: computed value at step `n + 1`
@@ -76,33 +83,37 @@ pub fn wave_step(u: &[f64], v: &[f64], w: &mut [f64], dim: (usize, usize), mu: f
         let s3 = nx * min(2 * i + 2, ny - 1);
 
         if simd_nx > 0 {
-        // initialize data
-        let mut v1 = f64x4::load(v, s1);
-        let mut v2 = f64x4::load(v, s2);
-        let mut vp1 = f64x4::splat(v[s1]);
-        let mut vp2 = f64x4::splat(v[s2]);
-        for j in 0..simd_nx {
-            unsafe {
-                let v0 = load_unchecked(v, s0 + j * 4);
-                let vn1 = load_unchecked(v, s1 + j * 4 + 4);
-                let vn2 = load_unchecked(v, s2 + j * 4 + 4);
-                let v3 = load_unchecked(v, s3 + j * 4);
-                let u1 = load_unchecked(u, s1 + j * 4);
-                let u2 = load_unchecked(u, s2 + j * 4);
-                let vl1 = f64x4::new(vp1.extract(3), v1.extract(0), v1.extract(1), v1.extract(2));
-                let vl2 = f64x4::new(vp2.extract(3), v2.extract(0), v2.extract(1), v2.extract(2));
-                let vr1 = f64x4::new(v1.extract(1), v1.extract(2), v1.extract(3), vn1.extract(0));
-                let vr2 = f64x4::new(v2.extract(1), v2.extract(2), v2.extract(3), vn2.extract(0));
-                let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v2);
-                let w2 = cc * v2 - u2 + co * (vl2 + vr2 + v1 + v3);
-                store_unchecked(w1, w, s1 + j * 4);
-                store_unchecked(w2, w, s2 + j * 4);
-                vp1 = v1;
-                vp2 = v2;
-                v1 = vn1;
-                v2 = vn2;
+            // initialize data
+            let mut v1 = f64x4::load(v, s1);
+            let mut v2 = f64x4::load(v, s2);
+            let mut vp1 = f64x4::splat(v[s1]);
+            let mut vp2 = f64x4::splat(v[s2]);
+            for j in 0..simd_nx {
+                unsafe {
+                    let v0 = load_unchecked(v, s0 + j * 4);
+                    let vn1 = load_unchecked(v, s1 + j * 4 + 4);
+                    let vn2 = load_unchecked(v, s2 + j * 4 + 4);
+                    let v3 = load_unchecked(v, s3 + j * 4);
+                    let u1 = load_unchecked(u, s1 + j * 4);
+                    let u2 = load_unchecked(u, s2 + j * 4);
+                    let vl1 =
+                        f64x4::new(vp1.extract(3), v1.extract(0), v1.extract(1), v1.extract(2));
+                    let vl2 =
+                        f64x4::new(vp2.extract(3), v2.extract(0), v2.extract(1), v2.extract(2));
+                    let vr1 =
+                        f64x4::new(v1.extract(1), v1.extract(2), v1.extract(3), vn1.extract(0));
+                    let vr2 =
+                        f64x4::new(v2.extract(1), v2.extract(2), v2.extract(3), vn2.extract(0));
+                    let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v2);
+                    let w2 = cc * v2 - u2 + co * (vl2 + vr2 + v1 + v3);
+                    store_unchecked(w1, w, s1 + j * 4);
+                    store_unchecked(w2, w, s2 + j * 4);
+                    vp1 = v1;
+                    vp2 = v2;
+                    v1 = vn1;
+                    v2 = vn2;
+                }
             }
-        }
         }
         for j in simd_nx * 4..nx {
             let jl = j.saturating_sub(1);
@@ -120,22 +131,24 @@ pub fn wave_step(u: &[f64], v: &[f64], w: &mut [f64], dim: (usize, usize), mu: f
         let s2 = s1;
 
         if simd_nx > 0 {
-        // initialize data
-        let mut v1 = f64x4::load(v, s1);
-        let mut vp1 = f64x4::splat(v[s1]);
-        for j in 0..simd_nx {
-            unsafe {
-                let v0 = load_unchecked(v, s0 + j * 4);
-                let vn1 = load_unchecked(v, s1 + j * 4 + 4);
-                let u1 = load_unchecked(u, s1 + j * 4);
-                let vl1 = f64x4::new(vp1.extract(3), v1.extract(0), v1.extract(1), v1.extract(2));
-                let vr1 = f64x4::new(v1.extract(1), v1.extract(2), v1.extract(3), vn1.extract(0));
-                let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v1);
-                store_unchecked(w1, w, s1 + j * 4);
-                vp1 = v1;
-                v1 = vn1;
+            // initialize data
+            let mut v1 = f64x4::load(v, s1);
+            let mut vp1 = f64x4::splat(v[s1]);
+            for j in 0..simd_nx {
+                unsafe {
+                    let v0 = load_unchecked(v, s0 + j * 4);
+                    let vn1 = load_unchecked(v, s1 + j * 4 + 4);
+                    let u1 = load_unchecked(u, s1 + j * 4);
+                    let vl1 =
+                        f64x4::new(vp1.extract(3), v1.extract(0), v1.extract(1), v1.extract(2));
+                    let vr1 =
+                        f64x4::new(v1.extract(1), v1.extract(2), v1.extract(3), vn1.extract(0));
+                    let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v1);
+                    store_unchecked(w1, w, s1 + j * 4);
+                    vp1 = v1;
+                    v1 = vn1;
+                }
             }
-        }
         }
         for j in simd_nx * 4..nx {
             let jl = j.saturating_sub(1);
@@ -144,6 +157,142 @@ pub fn wave_step(u: &[f64], v: &[f64], w: &mut [f64], dim: (usize, usize), mu: f
                         mu * (v[s1 + jl] + v[s1 + jr] + v[s0 + j] + v[s2 + j] - 4. * v[s1 + j]);
         }
     }
+}
+
+pub fn wave_step_single(u: &[f64], v: &[f64], w: &mut [f64], dim: (usize, usize), mu: f64) {
+    let (ny, nx) = dim;
+    let n = nx * ny;
+    assert_eq!(u.len(), n);
+    assert_eq!(v.len(), n);
+    assert_eq!(w.len(), n);
+
+    let simd_nx = (nx / 4).saturating_sub(1);
+
+    let co = f64x4::splat(mu);
+    let cc = f64x4::splat(2. - 4. * mu);
+
+    for i in 0..ny {
+        let s0 = nx * i.saturating_sub(1);
+        let s1 = nx * i;
+        let s2 = nx * min(i + 1, ny - 1);
+
+        if simd_nx > 0 {
+            // initialize data
+            let mut v1 = f64x4::load(v, s1);
+            let mut vp1 = f64x4::splat(v[s1]);
+            for j in 0..simd_nx {
+                unsafe {
+                    let v0 = load_unchecked(v, s0 + j * 4);
+                    let vn1 = load_unchecked(v, s1 + j * 4 + 4);
+                    let v2 = load_unchecked(v, s2 + j * 4);
+                    let u1 = load_unchecked(u, s1 + j * 4);
+                    let vl1 =
+                        f64x4::new(vp1.extract(3), v1.extract(0), v1.extract(1), v1.extract(2));
+                    let vr1 =
+                        f64x4::new(v1.extract(1), v1.extract(2), v1.extract(3), vn1.extract(0));
+                    let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v2);
+                    store_unchecked(w1, w, s1 + j * 4);
+                    vp1 = v1;
+                    v1 = vn1;
+                }
+            }
+        }
+        for j in simd_nx * 4..nx {
+            let jl = j.saturating_sub(1);
+            let jr = std::cmp::min(j + 1, nx - 1);
+            w[s1 + j] = 2. * v[s1 + j] - u[s1 + j] +
+                        mu * (v[s1 + jl] + v[s1 + jr] + v[s0 + j] + v[s2 + j] - 4. * v[s1 + j]);
+        }
+    }
+}
+
+pub fn wave_step_parallel(u: &[f64], v: &[f64], w: &mut [f64], dim: (usize, usize), mu: f64) {
+    let (ny, nx) = dim;
+    let n = nx * ny;
+    assert_eq!(u.len(), n);
+    assert_eq!(v.len(), n);
+    assert_eq!(w.len(), n);
+
+    struct Env {
+        nx: usize,
+        ny: usize,
+        simd_nx: usize,
+        co: f64x4,
+        cc: f64x4,
+        mu: f64,
+    };
+
+
+    let env = Env {
+        nx: nx,
+        ny: ny,
+        simd_nx: (nx / 4).saturating_sub(1),
+
+        co: f64x4::splat(mu),
+        cc: f64x4::splat(2. - 4. * mu),
+        mu: mu,
+    };
+
+
+    // find optimal num of rows per thread
+
+    fn inner(env: &Env, u: &[f64], v: &[f64], w: &mut [f64], row_start: usize, row_end: usize) {
+        let nx = env.nx;
+        let ny = env.ny;
+        let simd_nx = env.simd_nx;
+        let co = env.co;
+        let cc = env.cc;
+        let mu = env.mu;
+
+        let elems_per_thread = 200 * 1024;
+        if row_end - row_start > 1 && w.len() > elems_per_thread {
+            let mid = (row_start + row_end) / 2;
+            let (left, right) = w.split_at_mut((mid - row_start) * nx);
+            rayon::join(|| inner(env, u, v, left, row_start, mid),
+                        || inner(env, u, v, right, mid, row_end));
+        } else {
+            for i in row_start..row_end {
+                let s0 = nx * i.saturating_sub(1);
+                let s1 = nx * i;
+                let s2 = nx * min(i + 1, ny - 1);
+
+                if simd_nx > 0 {
+                    // initialize data
+                    let mut v1 = f64x4::load(v, s1);
+                    let mut vp1 = f64x4::splat(v[s1]);
+                    for j in 0..simd_nx {
+                        unsafe {
+                            let v0 = load_unchecked(v, s0 + j * 4);
+                            let vn1 = load_unchecked(v, s1 + j * 4 + 4);
+                            let v2 = load_unchecked(v, s2 + j * 4);
+                            let u1 = load_unchecked(u, s1 + j * 4);
+                            let vl1 = f64x4::new(vp1.extract(3),
+                                                 v1.extract(0),
+                                                 v1.extract(1),
+                                                 v1.extract(2));
+                            let vr1 = f64x4::new(v1.extract(1),
+                                                 v1.extract(2),
+                                                 v1.extract(3),
+                                                 vn1.extract(0));
+                            let w1 = cc * v1 - u1 + co * (vl1 + vr1 + v0 + v2);
+                            store_unchecked(w1, w, s1 + j * 4 - row_start * nx);
+                            vp1 = v1;
+                            v1 = vn1;
+                        }
+                    }
+                }
+                for j in simd_nx * 4..nx {
+                    let jl = j.saturating_sub(1);
+                    let jr = std::cmp::min(j + 1, nx - 1);
+                    w[s1 + j - row_start * nx] =
+                        2. * v[s1 + j] - u[s1 + j] +
+                        mu * (v[s1 + jl] + v[s1 + jr] + v[s0 + j] + v[s2 + j] - 4. * v[s1 + j]);
+                }
+            }
+        }
+    };
+
+    inner(&env, u, v, w, 0, ny);
 }
 
 #[cfg(test)]
@@ -168,14 +317,68 @@ mod test {
         wave_step(u.as_slice().unwrap(),
                   v.as_slice().unwrap(),
                   w.as_slice_mut().unwrap(),
-                  dim, mu);
+                  dim,
+                  mu);
 
         let d = &w - &w_ref;
         let err = (&d * &d).scalar_sum();
         if err > 1e-10 {
             println!("{}\n", w);
             println!("{}\n", w_ref);
-            println!("{}", d.mapv(|x| {if x.abs() < 1e-14 { 0. } else { x }}));
+            println!("{}",
+                     d.mapv(|x| {
+                if x.abs() < 1e-14 {
+                    0.
+                } else {
+                    x
+                }
+            }));
+            panic!("Error too big: {}", err);
+        }
+
+        let mut w = Array::zeros(dim);
+        wave_step_single(u.as_slice().unwrap(),
+                         v.as_slice().unwrap(),
+                         w.as_slice_mut().unwrap(),
+                         dim,
+                         mu);
+
+        let d = &w - &w_ref;
+        let err = (&d * &d).scalar_sum();
+        if err > 1e-10 {
+            println!("{}\n", w);
+            println!("{}\n", w_ref);
+            println!("{}",
+                     d.mapv(|x| {
+                if x.abs() < 1e-14 {
+                    0.
+                } else {
+                    x
+                }
+            }));
+            panic!("Error too big: {}", err);
+        }
+
+        let mut w = Array::zeros(dim);
+        wave_step_parallel(u.as_slice().unwrap(),
+                           v.as_slice().unwrap(),
+                           w.as_slice_mut().unwrap(),
+                           dim,
+                           mu);
+
+        let d = &w - &w_ref;
+        let err = (&d * &d).scalar_sum();
+        if err > 1e-10 {
+            println!("{}\n", w);
+            println!("{}\n", w_ref);
+            println!("{}",
+                     d.mapv(|x| {
+                if x.abs() < 1e-14 {
+                    0.
+                } else {
+                    x
+                }
+            }));
             panic!("Error too big: {}", err);
         }
     }
