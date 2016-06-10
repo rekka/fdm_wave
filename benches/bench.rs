6de54038197665a2017fff209c3aca6cdb1d5991
Norbert Pozar <bertapozar@gmail.com>
2016-06-10 13:06:26 +0900

    Fix benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,41 +1,41 @@
 #![feature(test)]
 extern crate test;
-extern crate ndarray;
+// extern crate ndarray;
 extern crate fdm_wave;
 
 mod bench {
-    mod ndarray {
-        use test::{black_box, Bencher};
-        use ndarray::Array;
-        use fdm_wave::*;
-
-        fn bench_ndarray(b: &mut Bencher, ny: usize, nx: usize) {
-            let dim = (ny, nx);
-            let u = Array::from_elem(dim, 1.);
-            let v = black_box(Array::from_elem(dim, 1.));
-            let mut w = Array::from_elem(dim, 1.);
-            let mu = 2.32;
-
-            b.iter(|| {
-                wave_step_ndarray(&u, &v, &mut w, mu);
-            });
-        }
-
-        #[bench]
-        fn s128(b: &mut Bencher) {
-            bench_ndarray(b, 128, 128);
-        }
-
-        #[bench]
-        fn s512(b: &mut Bencher) {
-            bench_ndarray(b, 512, 512);
-        }
-
-        #[bench]
-        fn s2048(b: &mut Bencher) {
-            bench_ndarray(b, 2048, 2048);
-        }
-    }
+    // mod ndarray {
+    //     use test::{black_box, Bencher};
+    //     use ndarray::Array;
+    //     use fdm_wave::*;
+    //
+    //     fn bench_ndarray(b: &mut Bencher, ny: usize, nx: usize) {
+    //         let dim = (ny, nx);
+    //         let u = Array::from_elem(dim, 1.);
+    //         let v = black_box(Array::from_elem(dim, 1.));
+    //         let mut w = Array::from_elem(dim, 1.);
+    //         let mu = 2.32;
+    //
+    //         b.iter(|| {
+    //             wave_step_ndarray(&u, &v, &mut w, mu);
+    //         });
+    //     }
+    //
+    //     #[bench]
+    //     fn s128(b: &mut Bencher) {
+    //         bench_ndarray(b, 128, 128);
+    //     }
+    //
+    //     #[bench]
+    //     fn s512(b: &mut Bencher) {
+    //         bench_ndarray(b, 512, 512);
+    //     }
+    //
+    //     #[bench]
+    //     fn s2048(b: &mut Bencher) {
+    //         bench_ndarray(b, 2048, 2048);
+    //     }
+    // }
 
     macro_rules! bench_step {
         ($modname:ident, $func: ident, $(($name:ident, $ny:expr, $nx:expr))+) => {
@@ -62,7 +62,7 @@ mod bench {
         }
     }
 
-    bench_step!{simd, wave_step,
+    bench_step!{simd_single, wave_step,
                 (s128, 128, 128)
                 (s512, 512, 512)
                 (s2048, 2048, 2048)
